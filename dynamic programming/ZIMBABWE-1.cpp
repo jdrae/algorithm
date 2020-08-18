45e6ae8a0ecebe99e9834c7a9b4bb9a36ff835f6
jdrae <dr.kiwi.j@gmail.com>
2020-08-18 20:23:02 +0900

    13 algospot

---
+++ dynamic programming/ZIMBABWE-1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+bool isBitSet(unsigned long long a, int b) {
+	return (a & (1ull << b)) > 0;
+}
+
+int main() {
+	
+}
