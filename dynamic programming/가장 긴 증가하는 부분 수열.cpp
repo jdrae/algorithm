e685dbd5cad9a2d5a7bf708dc786fef7c9dc47ab
jdrae <dr.kiwi.j@gmail.com>
2020-09-15 20:57:39 +0900

    17 baekjoon 11053

---
+++ dynamic programming/가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1000];
+int cache[1000];
+
+int lis(int start) {
+	if (start == n) return 1;
+	int& ret = cache[start+1];
+	if (ret != -1) return ret;
+
+	ret = 1; // wae!!
+	for (int next = start+1; next < n; next++) {
+		if (start == -1 || arr[start] < arr[next]) {
+			ret = max(ret, lis(next) + 1);
+		}
+	}
+	return ret;
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	memset(cache, -1, sizeof(cache));
+	cout << lis(-1) -1 << endl; //WAE!!!
+}
