109872fd7bb2d14b1bbba9bb68b21c7168ed1cc0
jdrae <dr.kiwi.j@gmail.com>
2020-09-16 19:13:46 +0900

    18 baekjoon 12865

---
+++ dynamic programming/평범한 배낭.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+struct Prop {
+	int w; int v;
+};
+
+int n, k;
+Prop arr[100];
+int cache[100][100001];
+
+int value(int pos, int weight) {
+	if (pos == n) return 0;
+	
+	int& ret = cache[pos][weight];
+	if (ret != -1) return ret;
+
+	ret = value(pos + 1, weight);
+	if (arr[pos].w <= weight)
+		ret = max(ret, arr[pos].v + value(pos + 1, weight - arr[pos].w));
+	
+
+	return ret;
+}
+
+int main() {
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i].w >> arr[i].v;
+	memset(cache, -1, sizeof(cache));
+	cout << value(0, k) << endl;
+}
