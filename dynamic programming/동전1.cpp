30d7292be3fee874f48b8510a59326e76b2b8b49
jdrae <dr.kiwi.j@gmail.com>
2020-10-04 13:04:15 +0900

    22 baekjoon 2293 hmm..

---
+++ dynamic programming/동전1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+int arr[100];
+int dp[10001] = { 0, };
+
+int main() {
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	dp[0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = arr[i]; j <= k; j++) {
+			if (j - arr[i] >= 0)
+				dp[j] += dp[j - arr[i]];
+		}
+	}
+	cout << dp[k] << endl;
+}
