3b2fc03a0ac873c0d36af4efdec33cd371e97c70
jdrae <dr.kiwi.j@gmail.com>
2020-11-04 12:01:00 +0900

    32 algospot LIS - use -1

---
+++ dynamic programming/LIS.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int n, arr[500], cache[501];
+
+int func(int idx) {
+    // if (idx == n) return 0; //안해도 for 문에서 걸러짐
+    int& ret = cache[idx+1];
+    if (ret != -1) return ret;
+    ret = 0;
+    for (int i = idx + 1; i < n; i++) {
+        if (idx == -1 || arr[idx] < arr[i]) {
+            ret = max(ret, func(i) + 1);
+        }
+    }
+    return ret;
+}
+
+int main()
+{
+    int C; cin >> C;
+    while (C--) {
+        cin >> n;
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        memset(cache, -1, sizeof(cache));
+        cout << func(-1) << endl;
+    }
+}
