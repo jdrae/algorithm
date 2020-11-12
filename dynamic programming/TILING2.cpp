38fb1657c0c9868fe1d2e38705a98161fd668432
jdrae <dr.kiwi.j@gmail.com>
2020-11-12 11:45:25 +0900

    38 algospot TILING2 - fibonacci

---
+++ dynamic programming/TILING2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int MOD = 1000000007;
+int n, cache[101];
+
+int func(int tile) {
+    if (tile <= 1) return 1;
+    int& ret = cache[tile];
+    if (ret != -1) return ret;
+    return ret = (func(tile - 1) + func(tile - 2)) % MOD;
+}
+
+int main()
+{
+    int C; cin >> C;
+    while (C--) {
+        cin >> n;
+        memset(cache, -1, sizeof(cache));
+        cout << func(n) << endl;
+    }
+}
