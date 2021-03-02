0caa904604d754e87a5a8e3aa8085c09772eb420
jdrae <dr.kiwi.j@gmail.com>
2021-03-02 23:13:55 +0900

    45 baekjoon 14501

---
+++ dynamic programming/퇴사.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm> 
+#include <cstring>
+using namespace std;
+
+int N;
+int T[15], P[15];
+int dp[15];
+
+int func(int idx) {
+	if (N <= idx) return 0;
+	int& ret = dp[idx];
+	if (ret != -1) return ret;
+
+	ret = func(idx + 1);
+	if (idx + T[idx] <= N)
+		ret = max(P[idx] + func(idx + T[idx]), ret);
+	return ret;
+}
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> T[i] >> P[i];
+	}
+	memset(dp, -1, sizeof(dp));
+	cout << func(0);
+}
